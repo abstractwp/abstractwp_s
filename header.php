83ba14c2e6724d007cebff0baebaf3dc4fa23fbd
DanhThong <danhthong@gmail.com>
2023-04-25 17:50:48 +0700

    feat: add acf fields for gw submit to access
    chore: add jquery on header

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+	<?php wp_enqueue_script("jquery"); ?>
 	<?php wp_head(); ?>
 
 </head>
