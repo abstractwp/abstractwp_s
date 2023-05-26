7f7274210d751af70bc9238d8e75722c7db47e20
Danh Thong <danhthong@gmail.com>
2023-05-26 23:51:40 +0700

    feat: rewrite throughts slug

---
+++ inc/hooks/rewrite-throughts-slug.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @package    wd_s
+ * @subpackage theme
+ * @author     Thong Dang
+ */
+
+namespace WebDevStudios\wd_s;
+
+/**
+ * Remove base category slug on throughts category.
+ */
+function remove_category_base() {
+	$category_slug = 'throughts';
+
+	$category = get_category_by_slug( $category_slug );
+
+	if ( $category ) {
+		add_rewrite_rule(
+			'^' . $category_slug . '/?$',
+			'index.php?category_name=' . $category_slug,
+			'top'
+		);
+	}
+}
+add_action( 'init', __NAMESPACE__ . '\remove_category_base' );
