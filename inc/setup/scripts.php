a5a691f008fb3be3d2f4092faaa8214c9bdc707d
Danh Thong <danhthong@gmail.com>
2023-06-27 11:28:43 +0700

    fix: time to read on mobile

--- inc/setup/scripts.php
+++ inc/setup/scripts.php
@@ -19,7 +19,7 @@ function scripts() {
 		$asset_file = include $asset_file_path;
 	} else {
 		$asset_file = [
-			'version'      => '1.1.6',
+			'version'      => '1.1.7',
 			'dependencies' => [ 'wp-polyfill' ],
 		];
 	}
