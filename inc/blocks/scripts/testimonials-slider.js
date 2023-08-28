ba783ec3a85e20769e6e70aa45e05a1d7ce2067d
Danh Thong <danhthong@gmail.com>
2023-08-28 17:27:20 +0700

    feat: testimonial right edge item

--- inc/blocks/scripts/testimonials-slider.js
+++ inc/blocks/scripts/testimonials-slider.js
@@ -1,13 +1,26 @@
 'use strict';
 ( function ( $ ) {
 	$( document ).ready( function () {
-		$( '.testimonial-slider' ).slick( {
+		var slider = $('.testimonial-slider');
+
+		// slider.on('init reInit afterChange', function(event, slick, currentSlide) {
+		// 	var slides = slider.find('.testimonial-item');
+
+		// 	slides.removeClass('previous-slide');
+
+		// 	for (var i = 0; i < currentSlide; i++) {
+		// 		slides.eq(i).addClass('previous-slide');
+		// 	}
+		// });
+
+		slider.slick( {
 			infinite: false,
 			autoplay: false,
 			autoplaySpeed: 5000,
 			dots: false,
-			prevArrow: $('.slick-prev'),
-			nextArrow: $('.slick-next'),
+			prevArrow: $( '.slick-prev' ),
+			nextArrow: $( '.slick-next' )
 		} );
 	} );
+
 } )( jQuery );
--- template-parts/blocks/testimonials-slider.php
+++ template-parts/blocks/testimonials-slider.php
@@ -11,18 +11,17 @@ namespace WebDevStudios\wd_s;
 
 $wd_s_testimonials = get_field( 'testimonials' );
 $wd_s_block_title  = get_field( 'title' );
-
 echo '<div class="testimonial-block">';
 
 if ( $wd_s_testimonials ) :
-	echo '<div class="left-nav">';
+	echo '<div class="left-container"><div class="left-content">';
 	if ( $wd_s_testimonials ) :
 		echo '<h3>' . esc_html( $wd_s_block_title ) . '</h3>';
 		endif;
-		echo '<button type="button" class="slick-prev"><svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.4286 23.8454L12.5714 16.9882L19.4286 10.1311" stroke="#14353C" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg></button>';
-		echo '<button type="button" class="slick-next"><svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.5714 23.8454L19.4286 16.9882L12.5714 10.1311" stroke="#14353C" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg></button>';
+		echo '<div><button type="button" class="slick-prev"><svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.4286 23.8454L12.5714 16.9882L19.4286 10.1311" stroke="#14353C" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg></button>';
+		echo '<button type="button" class="slick-next"><svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.5714 23.8454L19.4286 16.9882L12.5714 10.1311" stroke="#14353C" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg></button></div>';
 
-	echo '</div>';
+	echo '</div></div>';
 	echo '<div class="testimonial-slider-container">';
 		echo '<div class="testimonial-slider">';
 	foreach ( $wd_s_testimonials as $wd_s_testimonial ) :
