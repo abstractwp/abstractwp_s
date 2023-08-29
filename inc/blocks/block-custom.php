1455994cac1dac045c5f9d14594b53f6be200102
Danh Thong <danhthong@gmail.com>
2023-08-29 20:06:29 +0700

    feat: add content slider block

--- inc/blocks/block-custom.php
+++ inc/blocks/block-custom.php
@@ -39,6 +39,28 @@ function collectivewp_register_blocks() {
 				},
 			)
 		);
+
+		// register testimonials slider block.
+		acf_register_block_type(
+			array(
+				'name'            => 'content-slider-block',
+				'title'           => __( 'CollectiveWP - Contents slider', 'wd_s' ),
+				'description'     => __( 'CollectiveWP - A slider of Contents', 'wd_s' ),
+				'render_template' => 'template-parts/blocks/contents-slider.php',
+				'category'        => 'collectivewp-blocks',
+				'icon'            => 'embed-generic',
+				'keywords'        => array( 'text', 'slider', 'collectivewp' ),
+				'mode'            => 'edit',
+				'supports'        => array(
+					'align' => false,
+					'mode'  => false,
+				),
+				'enqueue_assets'  => function() {
+					wp_enqueue_script( 'slick-script', get_template_directory_uri() . '/inc/blocks/scripts/slick.min.js', array( 'jquery' ), '1.8.1', true );
+					wp_enqueue_script( 'contents-script', get_template_directory_uri() . '/inc/blocks/scripts/contents-slider.js', array( 'jquery', 'slick-script' ), '1.0', true );
+				},
+			)
+		);
 	}
 }
 
