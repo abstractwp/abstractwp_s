8d59d8f4c6567d291e0e110ec3956346663694e0
Danh Thong <danhthong@gmail.com>
2023-09-01 12:39:43 +0700

    refactor: only render duotone on page needed

--- inc/functions/get-dualtone.php
+++ inc/functions/get-dualtone.php
@@ -13,5 +13,13 @@ namespace WebDevStudios\wd_s;
  * @author Thong Dang
  */
 function get_dualtone_colors() {
+	// Render doutone filter.
+	if ( function_exists( 'wp_get_global_styles_svg_filters' ) ) {
+		$filters = wp_get_global_styles_svg_filters();
+		if ( ! empty( $filters ) ) {
+			echo $filters; // phpcs:ignore
+		}
+	}
+
 	return [ 'secondary-primary', 'secondary-tertiary', 'secondary-yellow', 'secondary-gray', 'primary-base', 'primary-tertiary', 'primary-yellow', 'primary-gray', 'yellow-gray', 'yellow-tertiary' ];
 }
