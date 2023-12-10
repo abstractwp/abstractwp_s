49ed0c04a63bb08623007078e71a47dcd01868bc
Danh Thong <danhthong@gmail.com>
2023-12-10 12:48:23 +0700

    feat: cta pattern

---
+++ patterns/cta.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Title: Call to Action
+ * Slug: wds/cta
+ * Categories: wds-patterns
+ *
+ * @package wd_s
+ */
+
+?>
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/inner-banner.jpg","id":129,"dimRatio":40,"overlayColor":"black","focalPoint":{"x":0.5,"y":0.54},"align":"full"} -->
+<div class="wp-block-cover alignfull"><span aria-hidden="true" class="wp-block-cover__background has-black-background-color has-background-dim-40 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-129" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/inner-banner.jpg" style="object-position:50% 54%" data-object-fit="cover" data-object-position="50% 54%"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"align":"wide","layout":{"inherit":false}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"textAlign":"center","level":5,"textColor":"white"} -->
+<h5 class="wp-block-heading has-text-align-center has-white-color has-text-color">Subheading goes here</h5>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Content Section Headline Goes Here</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"className":"","layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textAlign":"left","className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-text-align-left wp-element-button" href="#">Get in touch</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Call Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
