4e33f3b8e20fdc490620abe3fd73affa0f918414
Danh Thong <danhthong@gmail.com>
2023-12-11 17:57:30 +0700

    feat: add pricing boxes pattern

--- inc/hooks/register-block-pattern-category.php
+++ inc/hooks/register-block-pattern-category.php
@@ -40,6 +40,13 @@ function register_custom_block_pattern_category() {
 		]
 	);
 
+	register_block_pattern_category(
+		'wds-pricing',
+		[
+			'label' => __( 'Pricing', 'wd_s' ),
+		]
+	);
+
 	// Remove default patterns.
 	remove_theme_support( 'core-block-patterns' );
 }
