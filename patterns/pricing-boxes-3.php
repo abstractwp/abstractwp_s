a9436eaa82110a959610395348f4857c01371366
Danh Thong <danhthong@gmail.com>
2023-12-11 19:54:18 +0700

    feat: add pricing boxes 3,4 cols patterns

--- patterns/pricing-boxes-3.php
+++ patterns/pricing-boxes-3.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Title: Pricing Boxes
- * Slug: wds/pricing-boxes
+ * Title: Pricing Boxes 3 cols
+ * Slug: wds/pricing-boxes-3
  * Categories: wds-pricing
  *
  * @package wd_s
@@ -22,8 +22,38 @@
 <p class="has-text-align-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat, ipsum ac auctor ornare, nunc ligula scelerisque eros.</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"border":{"width":"2px"}},"borderColor":"primary-500"} -->
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"backgroundColor":"neutral-50","className":"pricing-box-hightlight","layout":{"type":"default"}} -->
+<div class="wp-block-column pricing-box-hightlight has-neutral-50-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:heading {"level":5} -->
+<h5 class="wp-block-heading">Plan A</h5>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading">$25 / <span>mo</span></h1>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li>Lorem ipsum dolor sit amet.</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet.</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Lorem ipsum dolor sit amet.</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Get Started</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"border":{"width":"2px"}},"borderColor":"primary-500"} -->
 <div class="wp-block-column has-border-color has-primary-500-border-color" style="border-width:2px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:heading {"level":5} -->
 <h5 class="wp-block-heading">Plan B</h5>
 <!-- /wp:heading -->
@@ -55,7 +85,7 @@
 
 <!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"backgroundColor":"neutral-50"} -->
 <div class="wp-block-column has-neutral-50-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:heading {"level":5} -->
-<h5 class="wp-block-heading">Plan B</h5>
+<h5 class="wp-block-heading">Plan C</h5>
 <!-- /wp:heading -->
 
 <!-- wp:heading {"level":1} -->
