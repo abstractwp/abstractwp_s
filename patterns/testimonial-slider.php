006b2f4224e80e0b3b5701141b64c84873e4e8a7
Danh Thong <danhthong@gmail.com>
2023-12-13 00:55:34 +0700

    feat: testimonial slider pattern

---
+++ patterns/testimonial-slider.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Testimonials slider
+ * Slug: wds/testimonials-slider
+ * Categories: wds-testimonial
+ *
+ * @package wd_s
+ */
+
+?>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"},"margin":{"top":"0","bottom":"0"}}},"className":"testimonial-image","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignfull testimonial-image" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:heading {"textAlign":"center","level":5} -->
+<h5 class="wp-block-heading has-text-align-center">Testimonials</h5>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Testimonials Section Headline Goes Here</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|60"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--60)"><!-- wp:acf/testimonials-slider-block {"name":"acf/testimonials-slider-block","data":{"field_65782b006f352":"center-slider","field_60978d67b0fad":{"row-0":{"field_64ebfedfaed14":"John Smith","field_64ebff19aed16":"CEO of Company","field_64ebfef6aed15":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu tincidunt libero. Vivamus odio est, malesuada eu sapien id, scelerisque vehicula lacus. Pellentesque pretium viverra lorem, sed iaculis ex feugiat non.","field_64f95ca1482b4":"109","field_65782491f1d1e":"0"},"row-1":{"field_64ebfedfaed14":"John Smith","field_64ebff19aed16":"CEO of Company","field_64ebfef6aed15":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu tincidunt libero. Vivamus odio est, malesuada eu sapien id, scelerisque vehicula lacus. Pellentesque pretium viverra lorem, sed iaculis ex feugiat non.","field_64f95ca1482b4":"109","field_65782491f1d1e":"4"},"65789c684289f":{"field_64ebfedfaed14":"John Smith","field_64ebff19aed16":"CEO of Company","field_64ebfef6aed15":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu tincidunt libero. Vivamus odio est, malesuada eu sapien id, scelerisque vehicula lacus. Pellentesque pretium viverra lorem, sed iaculis ex feugiat non.","field_64f95ca1482b4":"","field_65782491f1d1e":"3"}}},"mode":"preview"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
