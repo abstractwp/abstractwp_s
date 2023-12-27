fbcd19f311225f9670cd225d205c84dedf517dc0
Danh Thong <danhthong@gmail.com>
2023-12-27 16:56:49 +0700

    chore: accordion title open color

--- blocks/accordion/script.js
+++ blocks/accordion/script.js
@@ -7,27 +7,30 @@
 
 // add JS here.
 // Get all the accordion items
-const accordionItems = document.querySelectorAll('.accordion-item');
+const accordionItems = document.querySelectorAll( '.accordion-item' );
 
 // Add a click event listener to each accordion button
-accordionItems.forEach(item => {
-	const button = item.querySelector('.accordion-title');
-	const content = item.querySelector('.accordion-content');
+accordionItems.forEach( ( item ) => {
+	const button = item.querySelector( '.accordion-title' );
+	const content = item.querySelector( '.accordion-content' );
 
-	button.addEventListener('click', () => {
+	button.addEventListener( 'click', () => {
 		// Close all other accordion items
-		accordionItems.forEach(otherItem => {
-			if (otherItem !== item) {
-				const otherContent = otherItem.querySelector('.accordion-content');
-				const otherButton = otherItem.querySelector('.accordion-title');
+		accordionItems.forEach( ( otherItem ) => {
+			if ( otherItem !== item ) {
+				const otherContent =
+					otherItem.querySelector( '.accordion-content' );
+				const otherButton =
+					otherItem.querySelector( '.accordion-title' );
 				otherContent.style.display = 'none';
-				otherButton.classList.remove('open');
+				otherButton.classList.remove( 'open' );
 			}
-		});
+		} );
 
-		button.classList.add('open');
+		button.classList.add( 'open' );
 
 		// Toggle the display of the clicked item's content
-		content.style.display = content.style.display === 'block' ? 'none' : 'block';
-	});
-});
+		content.style.display =
+			content.style.display === 'block' ? 'none' : 'block';
+	} );
+} );
