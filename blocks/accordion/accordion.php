5e9a4317c96502772ddf124c04ab28175be037d8
Danh Thong <danhthong@gmail.com>
2023-12-27 18:07:14 +0700

    feat: allow accordion show on two cols

--- blocks/accordion/accordion.php
+++ blocks/accordion/accordion.php
@@ -16,6 +16,7 @@ if ( ! empty( $block['align'] ) ) {
 
 $wds_accordions      = get_field( 'accordions' );
 $wds_open_first_item = get_field( 'open_first_item' );
+$wds_is_two_columns  = get_field( 'is_two_columns' );
 
 // Add anchor to the block.
 $wds_anchor = ( ! empty( $block['anchor'] ) ) ? 'id="' . esc_attr( $block['anchor'] ) . '" ' : '';
@@ -26,13 +27,19 @@ $wds_anchor = ( ! empty( $block['anchor'] ) ) ? 'id="' . esc_attr( $block['ancho
 		?>
 	<div class="accordions">
 		<?php
-		$wds_i = 0;
+		$wds_i = 1;
 		foreach ( $wds_accordions as $wds_accordion ) :
+
+			if ( $wds_is_two_columns && 1 === $wds_i ) {
+				echo '<!-- wp:columns -->
+			<div class="wp-block-columns"><!-- wp:column -->
+			<div class="wp-block-column">';
+			}
 			?>
 		<div class="accordion-item">
 			<h4 class="accordion-title
 			<?php
-			if ( 0 === $wds_i && $wds_open_first_item ) {
+			if ( 1 === $wds_i && $wds_open_first_item ) {
 				echo 'open';}
 			?>
 			">
@@ -40,7 +47,7 @@ $wds_anchor = ( ! empty( $block['anchor'] ) ) ? 'id="' . esc_attr( $block['ancho
 			</h4>
 			<div class="accordion-content"
 			<?php
-			if ( 0 === $wds_i && $wds_open_first_item ) {
+			if ( 1 === $wds_i && $wds_open_first_item ) {
 				echo 'style="display:block"';}
 			?>
 			>
@@ -48,7 +55,20 @@ $wds_anchor = ( ! empty( $block['anchor'] ) ) ? 'id="' . esc_attr( $block['ancho
 			</div>
 		</div>
 			<?php
+
+			if ( $wds_is_two_columns && ceil( count( $wds_accordions ) / 2 ) == $wds_i ) {  //phpcs:ignore.
+				echo '</div>
+				<!-- /wp:column --><!-- wp:column -->
+				<div class="wp-block-column">
+				';
+			}
+
+			if ( $wds_is_two_columns && count( $wds_accordions ) === $wds_i ) {
+				echo '<!-- /wp:column --></div>
+				<!-- /wp:columns --></div>';
+			}
 			++$wds_i;
+
 		endforeach;
 		?>
 	</div>
