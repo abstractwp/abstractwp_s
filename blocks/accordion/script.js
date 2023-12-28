35f965586ada95a141e593a624f8fbefe4dddbbd
Danh Thong <danhthong@gmail.com>
2023-12-28 18:38:36 +0700

    fix: testimonial and accordion block not loaded the script on no-title template

--- blocks/accordion/script.js
+++ blocks/accordion/script.js
@@ -6,31 +6,33 @@
  */
 
 // add JS here.
-// Get all the accordion items
-const accordionItems = document.querySelectorAll( '.accordion-item' );
+document.addEventListener('DOMContentLoaded', () => {
+	// Get all the accordion items
+	const accordionItems = document.querySelectorAll( '.accordion-item' );
 
-// Add a click event listener to each accordion button
-accordionItems.forEach( ( item ) => {
-	const button = item.querySelector( '.accordion-title' );
-	const content = item.querySelector( '.accordion-content' );
+	// Add a click event listener to each accordion button
+	accordionItems.forEach( ( item ) => {
+		const button = item.querySelector( '.accordion-title' );
+		const content = item.querySelector( '.accordion-content' );
 
-	button.addEventListener( 'click', () => {
-		// Close all other accordion items
-		accordionItems.forEach( ( otherItem ) => {
-			if ( otherItem !== item ) {
-				const otherContent =
-					otherItem.querySelector( '.accordion-content' );
-				const otherButton =
-					otherItem.querySelector( '.accordion-title' );
-				otherContent.style.display = 'none';
-				otherButton.classList.remove( 'open' );
-			}
+		button.addEventListener( 'click', () => {
+			// Close all other accordion items
+			accordionItems.forEach( ( otherItem ) => {
+				if ( otherItem !== item ) {
+					const otherContent =
+						otherItem.querySelector( '.accordion-content' );
+					const otherButton =
+						otherItem.querySelector( '.accordion-title' );
+					otherContent.style.display = 'none';
+					otherButton.classList.remove( 'open' );
+				}
+			} );
+
+			button.classList.add( 'open' );
+
+			// Toggle the display of the clicked item's content
+			content.style.display =
+				content.style.display === 'block' ? 'none' : 'block';
 		} );
-
-		button.classList.add( 'open' );
-
-		// Toggle the display of the clicked item's content
-		content.style.display =
-			content.style.display === 'block' ? 'none' : 'block';
 	} );
 } );
--- blocks/testimonial/script.asset.php
+++ blocks/testimonial/script.asset.php
@@ -1,5 +1,5 @@
 <?php // phpcs:ignore Squiz.Commenting.FileComment.Missing
 return array(
-	'dependencies' => array(),
+	'dependencies' => array( 'jquery' ),
 	'version'      => false,
 );
