33071d3df1d6d362c672285a35be5a4462daa209
Danh Thong <danhthong@gmail.com>
2024-02-28 14:08:08 +0700

    chore: update header column width

--- patterns/header.php
+++ patterns/header.php
@@ -15,14 +15,14 @@ $wds_site_logo = has_custom_logo() ? '<!-- wp:site-logo /-->' : '<!-- wp:site-ti
 <div class="wp-block-group alignfull">
 	<!-- wp:columns {"verticalAlignment":"center","isStackedOnMobile":false,"align":"wide"} -->
 	<div class="wp-block-columns alignwide are-vertically-aligned-center is-not-stacked-on-mobile">
-		<!-- wp:column {"verticalAlignment":"center","width":"100%"} -->
-		<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:100%">
+		<!-- wp:column {"verticalAlignment":"center","width":"30%"} -->
+		<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:30%">
 			<?php echo wp_kses_post( $wds_site_logo ); ?>
 		</div>
 		<!-- /wp:column -->
 
-		<!-- wp:column {"verticalAlignment":"center","width":"100%"} -->
-		<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:100%">
+		<!-- wp:column {"verticalAlignment":"center","width":"70%"} -->
+		<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:70%">
 			<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"}} /-->
 		</div>
 		<!-- /wp:column -->
